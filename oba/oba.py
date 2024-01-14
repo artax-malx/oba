bd3b9f35d0edf4ce6a4ad131bd45b539e1aa9b97
Artagan Malsagov <artaganmalsagov@MacBook-Pro.fritz.box>
2024-01-14 21:48:52 +0100

    Made the main code run for all input files

--- oba/oba.py
+++ oba/oba.py
@@ -1,103 +1,214 @@
 import pandas as pd
 import numpy as np
+from sortedcontainers import SortedDict
+import itertools
+import time
+from logger import init_logger
 
-def get_data(date):
+logger = init_logger("./logs/oba.log")
 
-    df = pd.read_csv(f"./data/res_{date}.csv", sep = ",")
-    df = pd.read_csv(f"./data/test_input.csv", sep = ",")
+def get_data(date, test=False):
+    if test:
+        df = pd.read_csv("./data/test_input.csv", sep=",")
+    else:
+        df = pd.read_csv(f"./data/res_{date}.csv", sep=",")
 
     return df
 
+
+def print_ob_dict(input_dict):
+
+    print("Size | Bid Price")
+    for i in range(5):
+        print(input_dict[f"bq{i}"], " | ",input_dict[f"bp{i}"])
+
+    print("==="*30)
+    print("Size | Ask Price")
+    for i in range(5):
+        print(input_dict[f"aq{i}"], " | ",input_dict[f"ap{i}"])
+
 def aggregate_order_book(dict_orders):
+    """ Helper function that aggregates dictionary of live orders and 
+    returns 5 best levels of both sides of the order book
+    """
 
-    dfo = pd.DataFrame.from_dict(dict_orders, orient = "index")
+    dfo = pd.DataFrame.from_dict(dict_orders, orient="index")
 
-    bids = dfo.loc[dfo['side'] == "b"]
-    asks = dfo.loc[dfo['side'] == "a"]
+    bids = dfo.loc[dfo["side"] == "b"]
+    asks = dfo.loc[dfo["side"] == "a"]
 
-    bid_side = bids.groupby(by = ['price'])['quantity'].sum()
-    ask_side = asks.groupby(by = ['price'])['quantity'].sum()
-    
+    bid_side = bids.groupby(by=["price"])["quantity"].sum()
+    ask_side = asks.groupby(by=["price"])["quantity"].sum()
+
     bid_side = bid_side.sort_index(ascending=False)
     ask_side = ask_side.sort_index(ascending=True)
 
     b = bid_side.iloc[:5].reset_index()
     a = ask_side.iloc[:5].reset_index()
 
-    b = b.rename(columns = {'price' :'bid','quantity' : 'bid_quantity'})
-    a = a.rename(columns = {'price' :'ask','quantity' : 'ask_quantity'})
+    b = b.rename(columns={"price": "bid", "quantity": "bid_quantity"})
+    a = a.rename(columns={"price": "ask", "quantity": "ask_quantity"})
 
-    b = b.loc[:, ['bid_quantity','bid']].copy()
+    b = b.loc[:, ["bid_quantity", "bid"]].copy()
 
-    return b,a
+    return b, a
+
+
+def get_n_levels(n, bid_dict, ask_dict):
+    """ Return a dictionary with n best levels from bid_dict and ask_dict 
+
+    If there are less than n levels available, bq = 0 and bp = NaN is filled in
+    """
+
+    bid_keys = bid_dict.__reversed__()
+    bid_num = min(len(bid_dict), n)
+    bid_best = list(itertools.islice(bid_keys, bid_num))
+    bid_levels = [(price, bid_dict[price]) for price in bid_best]
+
+    level_dict = {}
+    for i in range(n):
+        if (i + 1) <= bid_num:
+            level_dict[f"bp{i}"] = bid_levels[i][0]
+            level_dict[f"bq{i}"] = bid_levels[i][1]
+        else:
+            level_dict[f"bp{i}"] = np.nan
+            level_dict[f"bq{i}"] = 0
+
+    ask_num = min(len(ask_dict), n)
+    ask_best = list(itertools.islice(ask_dict, ask_num))
+    ask_levels = [(price, ask_dict[price]) for price in ask_best]
+
+    for i in range(n):
+        if (i + 1) <= ask_num:
+            level_dict[f"ap{i}"] = ask_levels[i][0]
+            level_dict[f"aq{i}"] = ask_levels[i][1]
+        else:
+            level_dict[f"ap{i}"] = np.nan
+            level_dict[f"aq{i}"] = 0
+
+    return level_dict
 
 
 def process_order_updates(df):
-
-
-    bps = [f'bp{i}' for i in range(5)]
-    bqs = [f'bq{i}' for i in range(5)] 
-    aps = [f'ap{i}' for i in range(5)]
-    aqs = [f'aq{i}' for i in range(5)] 
-    
-    prices = bps + aps
-    quants = bqs + aqs
-
-    data = []
     curr_orders = {}
+    bid_dict = SortedDict()
+    ask_dict = SortedDict()
 
     for index, row in df.iterrows():
-
-        action = row['action']
-        ord_id = row['id']
-        timestamp = row['timestamp']
-        side = row['side']
-        price = row['price']
-        quantity = row['quantity']
+        action = row["action"]
+        ord_id = row["id"]
+        timestamp = row["timestamp"]
+        side = row["side"]
+        price = row["price"]
+        quantity = row["quantity"]
 
         if action == "a":
-            order = {'timestamp' : timestamp,
-                      'side' : side,
-                      'price' : price,
-                      'quantity' : quantity,
-                      }
-            curr_orders[ord_id] = order
-        elif action == "m":
-            if not ord_id in curr_orders.keys():
-                print(f"Order id {ord_id} not in the data")
+            if side == "b":
+                bid_dict[price] = bid_dict.get(price, 0) + quantity
+            elif side == "a":
+                ask_dict[price] = ask_dict.get(price, 0) + quantity
+            else:
+                logger.error(f"Order id {ord_id} has incorrect side input")
+                continue
 
-            order = {'timestamp' : timestamp,
-                      'side' : side,
-                      'price' : price,
-                      'quantity' : quantity,
-                      }
-            curr_orders[ord_id] = order
+            new_order = {
+                "timestamp": timestamp,
+                "side": side,
+                "price": price,
+                "quantity": quantity,
+            }
+
+            curr_orders[ord_id] = new_order
         elif action == "d":
+            curr_ord = curr_orders.get(ord_id, None)
+            if not curr_ord:
+                logger.error(f"Can't delete Order id {ord_id}; not in the data")
+                continue
+
+            #TODO: take side, price and quantity from current order
+            if side == "b":
+                bid_dict[price] = bid_dict.get(price, 0) - quantity
+                if bid_dict[price] == 0:
+                    del bid_dict[price]
+            elif side == "a":
+                ask_dict[price] = ask_dict.get(price, 0) - quantity
+                if ask_dict[price] == 0:
+                    del ask_dict[price]
+            else:
+                logger.error(f"Order id {ord_id} has incorrect side input")
+                continue
             del curr_orders[ord_id]
+        elif action == "m":
+            curr_ord = curr_orders.get(ord_id, None)
+            if not curr_ord:
+                logger.error(f"Order id {ord_id} not in the data")
+                continue
 
-        b, a = aggregate_order_book(curr_orders)
+            curr_side = curr_ord["side"]
+            curr_price = curr_ord["price"]
+            curr_quantity = curr_ord["quantity"]
 
-        print(pd.concat([b,a], axis = 1))
-        print("==="*30)
+            if curr_side == "b":
+                bid_dict[curr_price] = bid_dict.get(curr_price, 0) - curr_quantity
+                if bid_dict[curr_price] == 0:
+                    del bid_dict[curr_price]
+            elif curr_side == "a":
+                ask_dict[curr_price] = ask_dict.get(curr_price, 0) - curr_quantity
+                if ask_dict[curr_price] == 0:
+                    del ask_dict[curr_price]
+            else:
+                # TODO: In theory not necessary to check, but might not be a bad idea
+                pass
 
-        #temp_dict = {'timestamp' : row['timestamp'],
+            if side == "b":
+                bid_dict[price] = bid_dict.get(price, 0) + quantity
+            elif side == "a":
+                ask_dict[price] = ask_dict.get(price, 0) + quantity
+            else:
+                logger.error(f"Order id {ord_id} has incorrect side input")
+                continue
+
+            new_order = {
+                "timestamp": timestamp,
+                "side": side,
+                "price": price,
+                "quantity": quantity,
+            }
+
+            curr_orders[ord_id] = new_order
+
+        out_dict = get_n_levels(5, bid_dict, ask_dict)
+        print(out_dict)
+
+        # temp_dict = {'timestamp' : row['timestamp'],
         #             'price' : row['price'],
         #             'side' : row['side'],}
 
-        #for x in prices:
+        # for x in prices:
         #    temp_dict[x] = np.nan
 
-        #for x in quants:
+        # for x in quants:
         #    temp_dict[x] = 0
 
-        #data.append(temp_dict)
+        # data.append(temp_dict)
 
-    #ob = pd.DataFrame(data)
-    #return ob 
-    return curr_orders
+    # ob = pd.DataFrame(data)
+    # return ob
+    return curr_orders, out_dict
 
 
 if __name__ == "__main__":
-    df1 = get_data("20190610")
-    out = process_order_updates(df1)
 
+    input_dates = ["20190610","20190611", "20190612","20190613", "20190614"]
+    datestr = "20190614"
+    logger.info(f"Started Order Book Analysis Run for {datestr}")
+
+    df_res = get_data(datestr, test=False)
+
+    start = time.time()
+    last_order_dict, last_level_dict = process_order_updates(df_res)
+    end = time.time()
+    print(f"Run time {end - start} sec")
+
+    b,a = aggregate_order_book(last_order_dict)
+    print_ob_dict(last_level_dict)
